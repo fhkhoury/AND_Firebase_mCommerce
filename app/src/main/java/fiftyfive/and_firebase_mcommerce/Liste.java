1d43fc7b4a926274dbdc80e4abae698462ba70ea
fhkhoury <f.khouryat@gmail.com>
2017-07-28 03:42:42 +0200

    add an action bar to Liste screen

--- app/src/main/java/fiftyfive/and_firebase_mcommerce/Liste.java
+++ app/src/main/java/fiftyfive/and_firebase_mcommerce/Liste.java
@@ -3,6 +3,9 @@ package fiftyfive.and_firebase_mcommerce;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +19,9 @@ public class Liste extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         Button goToDetail = (Button) findViewById(R.id.goToDetail);
         goToDetail.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,15 +30,33 @@ public class Liste extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
 
-        Button infos = (Button) findViewById(R.id.informations);
-        infos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ajoute les entrées de menu_test à l'ActionBar
+        getMenuInflater().inflate(R.menu.menu_liste, menu);
+        return true;
+    }
+
+    //gère le click sur une action de l'ActionBar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_cart:
+                //Goto action cart
+                return true;
+            case R.id.action_informations:
                 Intent i = new Intent(Liste.this, Informations.class);
                 startActivity(i);
-            }
-        });
+                return true;
+            case R.id.action_legal:
+                //Open a webview with CGU
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
